29a18f32b2434703b6ae78ac779c81a5bc297d0f
toffeezzz <animieone56@gmail.com>
2026-08-26 08:23:30 +0800

    added a dev mode arg on launch

--- src/ame/__main__.py
+++ src/ame/__main__.py
@@ -1,12 +1,27 @@
+from argparse import Namespace
+import argparse
 import asyncio
 
 from rich.prompt import Prompt
 
 from ame.cli.interface import run_cli
+from ame.cli.renderer import console
+from ame.settings.settings import get_settings
 
 # Removes the default built-in suffix
 Prompt.prompt_suffix = ""
 
 
+def parse_args() -> Namespace:
+    parser = argparse.ArgumentParser(
+        prog="cli-agent", description="An agent/companion assitant"
+    )
+    _ = parser.add_argument("--dev", action="store_true", help="Enable developer mode")
+    return parser.parse_args()
+
+
 def main() -> None:
+    args = parse_args()
+    get_settings().dev_mode = args.dev
+    console.print(args.dev)
     asyncio.run(run_cli())
