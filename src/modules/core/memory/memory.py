296792986f6494df3b5eabfa4db0d6698c58f175
toffeezzz <animieone56@gmail.com>
2026-09-07 18:56:45 +0800

    [ame] Add registered_skills field and Skill import to memory models

--- src/modules/core/memory/memory.py
+++ src/modules/core/memory/memory.py
@@ -7,23 +7,25 @@ from modules.core.memory.models import Memory, MemorySession
 
 
 class MemoryManager:
-    _current_session: MemorySession
+    current_session: MemorySession
 
     def __init__(self, session: MemorySession) -> None:
-        self._current_session = session
+        self.current_session = session
 
     def load_session(self, session: MemorySession) -> None:
-        self._current_session = session
+        del self.current_session.memories[1:]
+        self.current_session.memories.extend(session.memories)
+        self.current_session.start_date = session.start_date
 
     async def save_session(self, path: str) -> None:
         await asyncio.to_thread(self._write_session, path)
 
     def _write_session(self, path: str) -> None:
         with open(path, "w", encoding="utf-8") as file:
-            _ = file.write(self._current_session.model_dump_json(indent=4))
+            _ = file.write(self.current_session.model_dump_json(indent=4))
 
     def add_message(self, message: ChatCompletionMessageParam) -> None:
-        timestamp = datetime.datetime.now(datetime.UTC).strftime("%D")
+        timestamp = datetime.datetime.now(datetime.UTC).strftime("%D %I:%M:%S %p")
         speaker = message.get("name") or ""
         memory = Memory(
             speaker=speaker,
@@ -31,7 +33,7 @@ class MemoryManager:
             timestamp=timestamp,
             importance_score=1,
         )
-        self._current_session.memories.append(memory)
+        self.current_session.memories.append(memory)
 
     def dump_messages(
         self, messages: list[tuple[str, ChatCompletionMessageParam]]
@@ -44,11 +46,11 @@ class MemoryManager:
                 timestamp=timestamp,
                 importance_score=1,
             )
-            self._current_session.memories.append(memory)
+            self.current_session.memories.append(memory)
 
     @property
     def messages(self) -> list[ChatCompletionMessageParam]:
         messages: list[ChatCompletionMessageParam] = []
-        for memory in self._current_session.memories:
+        for memory in self.current_session.memories:
             messages.append(memory.message)
         return messages
