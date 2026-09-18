ce5c59c84167fa1169cf7d4846f5c353b12e0cc1
toffeeezz <animeone56@gmail.com>
2026-09-18 12:21:15 +0800

    passed the config object

--- src/modules/gui/app.py
+++ src/modules/gui/app.py
@@ -6,14 +6,16 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
+from modules.config.models import Config
 from modules.gui.main.chat_panel import ChatPanel
 from modules.gui.main.left_panel import LeftPanel
 
 
 @final
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, config: Config | None = None):
         super().__init__()
+        self.config = config or Config()
         self.setWindowTitle("My App")
         self.resize(1200, 900)
 
@@ -22,7 +24,7 @@ class MainWindow(QMainWindow):
         hbox_layout.setContentsMargins(0, 0, 0, 0)
         hbox_layout.setSpacing(0)
 
-        self.left_panel = LeftPanel()
+        self.left_panel = LeftPanel(self.config)
         self.chat_panel = ChatPanel()
 
         hbox_layout.addWidget(self.left_panel)
